7f7af5259bc4ff34a0da53d72fe048201bd5266c
triet <pvmtriet232@gmail.com>
2023-04-02 13:53:40 +0700

    format

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 # Variable declarations
 
 variable "aws_region" {
-    description = "AWS region"
-    type = string
-    default = "us-west-2"
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
 }
